cbd747461c6e5af31b1ef81daead3976d857856a
vaidhiyanathanr <vaidhiyanathanr@cardiff.ac.uk>
2025-10-01 11:11:59 +0100

    Create Task2.java

---
+++ Task2.java
@@ -0,0 +1,15 @@
+
+public class Task2 {
+    /**
+     * TODO: It contains various format inconsistencies,
+     * compile-time and run-time errors, find them, fix them and execute the program.
+     */
+    Public Static Void Main ( String [] args {
+        bottles = 1;
+        int volume = "2";
+        int OneMillion = 1 ,000 ,000;
+        double threeAndAHalf = 3 1/2;
+        double canVolume = 12 * literPerOunce ;
+        double literPerOunce = 0.0296;
+        double balance = 101.73;
+        int dollars = balance ; }}

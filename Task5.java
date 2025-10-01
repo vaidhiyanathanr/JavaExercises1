fdb363e0ac9dd1d0a3461de0a4157d89bcb4241e
vaidhiyanathanr <vaidhiyanathanr@cardiff.ac.uk>
2025-10-01 11:12:48 +0100

    Create Task5.java

---
+++ Task5.java
@@ -0,0 +1,24 @@
+
+public class Task5 {
+    /**
+     * TODO:
+     * Write a program which can convert Pounds to Euros, and vice versa. Upon running the program
+     * the user should be asked if they wish to perform Option 1 of converting Pounds to Euros; or
+     * Option 2 of converting Euros to Pounds. After making this choice, the user should be prompted
+     * with a suitable text to input an amount. The program should then output the converted amount.
+     * An example run might be
+     * Welcome to the Currency Converter
+     * 1. Convert Pounds to Euros
+     * 2. Convert Euros to Pounds
+     * Option: 1
+     * Enter amount in pounds: 400.00
+     * 400.00 pounds is 543.47 Euros.
+     * where the user enters their choice on Line 4 and the amount of money they wish to convert on
+     * Line 5.
+     * For some reason the $ and e symbols seem to randomly cause problems when programming on
+     * some machines. Hence I suggest you simply avoid using them (use the acronyms GBP
+     * and EUR instead) .
+     * Try to use more methods and try avoiding using all methods as static.
+     */
+   
+}

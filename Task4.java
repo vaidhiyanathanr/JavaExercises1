50774177f22f6dbe89f30978d3c7ffb4f2c50054
vaidhiyanathanr <vaidhiyanathanr@cardiff.ac.uk>
2025-10-01 11:12:25 +0100

    Create Task4.java

---
+++ Task4.java
@@ -0,0 +1,17 @@
+
+public class Task4 {
+    /**
+     * TODO: This is an extension task, so do it if you wish
+     * Extend your solution to Task 3 (copy it first) so that a receipt is produced which looks like:
+     * Dwr Cymru Water - Receipt
+     * Bottles of Water: n
+     * Cost Without VAT: xx.xx pounds
+     * Total Cost With VAT: yy.yy pounds
+     * @see Use <a href="https://www.geeksforgeeks.org/java/format-specifiers-in-java">format specifiers</a>
+     * to align the text and print the money values to 2 decimal places.
+     * Use a constant for the company name.
+     * Updating the company name should automatically yield
+     * a centred title on the receipt. To accomplish this you can use format specifiers and a little
+     * maths. You should also store the receipt width (number of characters) in a constant.
+     */
+}

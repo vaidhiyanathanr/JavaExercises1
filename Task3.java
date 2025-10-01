7a5bef533e212067224906cfafc22493ce196f35
vaidhiyanathanr <vaidhiyanathanr@cardiff.ac.uk>
2025-10-01 11:12:13 +0100

    Create Task3.java

---
+++ Task3.java
@@ -0,0 +1,17 @@
+
+public class Task3 {
+    /**
+     * TODO:
+     * Write a program which asks the user how many bottles of water they wish to order (an integer).
+     * The price per bottle without VAT is 0:78 pounds (i.e., 78 pence) and VAT is 20%.
+     * The program should calculate the total price with and without VAT and produce the following
+     * output:
+     * Total Cost Without VAT: xx.xx pounds
+     * Total Cost With VAT: yy.yy pounds
+     * where xx.xx is the total cost without VAT in pounds (printed to 2 decimal places) and yy.yy
+     * is the total cost with VAT in pounds printed to 2 decimal places.
+     * Hint: You should use constants in the code, e.g., for VAT and the price per bottle.
+     * Note: Do not try to print the pound symbol - this can cause problems and can be fairly tricky
+     */
+
+}
